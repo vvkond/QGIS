9b7c6c59cdfac8e0b605b3f5c61d3947d154a153
vvkond <kondrashov.viktor@gmail.com>
2017-11-20 23:33:47 +0500

    Сохранение данных

--- qgis_pds_SaveMapsetToPDS.py
+++ qgis_pds_SaveMapsetToPDS.py
@@ -68,11 +68,12 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
                 self.groupFile = 'Surface_group.sql'
                 self.setFile = 'Surface_set.sql'
                 self.mapSetType = 4
+
+            if self.currentLayer.type() == QgsMapLayer.VectorLayer:
+                self.updateFieldsComboBox()
+            else:
                 self.mSaveAsComboBox.setEnabled(False)
                 self.mSetFieldsCroupBox.setEnabled(False)
-
-            if self.prop != 'qgis_surface':
-                self.updateFieldsComboBox()
 
             name = self.currentLayer.name()
             self.mGroupLineEdit.setText(name)
@@ -124,16 +125,19 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
 
         self.mSubsetFields.clear()
         self.mParameterFields.clear()
+        self.mKeyFields.clear()
 
         self.mSubsetFields.addItem('-')
         self.mParameterFields.addItem('-')
         for f in self.currentLayer.fields():
             self.mSubsetFields.addItem(f.name())
             self.mParameterFields.addItem(f.name())
+            self.mKeyFields.addItem(f.name())
 
         if self.mSubsetFields.count() > 0:
             self.mSubsetFields.setCurrentIndex(self.mSubsetFields.findText('subset_name'))
             self.mParameterFields.setCurrentIndex(self.mParameterFields.findText('parameter'))
+            self.mKeyFields.setCurrentIndex(self.mKeyFields.findText('subset_no'))
 
     def getGroupNo(self, mapSetName):
         mapSetNo = -1
@@ -181,14 +185,25 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
         return mapSetNo
 
     def resetMapSetType(self):
+        self.mapSetCpSource = 0
         if self.mSaveAsComboBox.currentIndex() == 0:
             self.mapSetType = 1
+            self.groupFile = 'ControlPoints_group.sql'
+            self.setFile = 'ControlPoints_set.sql'
         elif self.mSaveAsComboBox.currentIndex() == 1:
             self.mapSetType = 2
+            self.groupFile = "Faults_group.sql"
+            self.setFile = "Faults_set.sql"
         elif self.mSaveAsComboBox.currentIndex() == 2:
             self.mapSetType = 1
+            self.groupFile = "Contours_group.sql"
+            self.setFile = "Contours_set.sql"
+            self.mapSetCpSource = 4
         else:
             self.mapSetType = 3
+            self.groupFile = "Polygons_group.sql"
+            self.setFile = "Polygons_set.sql"
+
 
     def getMaxSetNumber(self, sql):
         records = self.db.execute(sql)
@@ -213,28 +228,32 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
 
         self.groupNo = self.getGroupNo(groupNameToSave)
 
+        # Set MAP_SET_TYPE
+        self.resetMapSetType()
+
         if self.groupNo >= 0:
             setNo = self.getSetNo(self.groupNo, groupNameToSave+'/'+setNameToSave)
             if setNo >= 0:
                 QtGui.QMessageBox.critical(self, self.tr(u'Save to PDS'), self.tr(u'Group/Set names already exists'))
                 return
 
-        if self.prop == 'qgis_surface':
-            print "Saving surface..."
-            return
-
-        #Set MAP_SET_TYPE
-        self.resetMapSetType()
-
+        #Get _NO numbers
         self.maxMapSetNo = self.getMaxSetNumber('select max(tig_map_set_no) from tig_map_set')
         self.maxMapSubsetNo = self.getMaxSetNumber('select max(tig_map_subset_no) from tig_map_subset')
         self.maxMapSetParameterNo = self.getMaxSetNumber('select max(tig_map_set_parameter_no) from TIG_MAP_SET_PARAM')
 
+        if self.currentLayer.type() == QgsMapLayer.RasterLayer:
+            print "Saving surface..."
+            return
+
+
         provider = self.currentLayer.dataProvider()
         self.subsetFieldName = self.mSubsetFields.currentText()
         self.parameterFieldName = self.mParameterFields.currentText()
+        self.keyFieldName = self.mKeyFields.currentText()
         self.subsetFieldIndex = provider.fieldNameIndex(self.subsetFieldName)
         self.parameterFieldIndex = provider.fieldNameIndex(self.parameterFieldName)
+        self.keyFieldIndex = provider.fieldNameIndex(self.keyFieldName)
 
         if self.subsetFieldIndex < 0:
             if QtGui.QMessageBox.question(self, self.tr(u'Save to PDS'), self.tr(u'Subset field name is not found. Proceed?'),
@@ -253,6 +272,14 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
                                "values (TIG_MAP_SET_SEQ.nextval, '{0}', {1}, {2})"
                                .format(groupNameToSave, self.groupNo, self.mapSetType)):
                 return
+
+        # Create new param set
+        self.paramNo = self.maxMapSetParameterNo + 1
+        sql1 = ("insert into tig_map_set_param (db_sldnid, tig_param_short_name, TIG_PARAM_LONG_NAME, "
+                "tig_map_set_no, tig_map_set_parameter_no, TIG_MAP_SET_CP_SOURCE ) "
+                "values (TIG_MAP_SET_PARAM_SEQ.nextval, '0', '{0}', {1}, {2}, {3})"
+                .format(setNameToSave, self.groupNo, self.paramNo, self.mapSetCpSource))
+        self.db.execute(sql1)
 
 
         isPointsGeom = True
@@ -293,11 +320,22 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
         pointsX = []
         pointsY = []
         params = []
-        subsetName = 'Qgis'
+        paramName = 'Qgis'
+        prevKey = None
+        key = None
         for f in features:
+            if self.keyFieldIndex >= 0:
+                key = f.attribute(self.keyFieldName)
+
+            if prevKey and prevKey != key:
+                self.writePoints(pointsX, pointsY, params, paramName)
+                pointsX = []
+                pointsY = []
+                params = []
+
             parameter = -9999
             if self.subsetFieldIndex >= 0:
-                subsetName = f.attribute(self.subsetFieldName)
+                paramName = f.attribute(self.subsetFieldName)
             if self.parameterFieldIndex >= 0:
                 parameter = f.attribute(self.parameterFieldName)
 
@@ -316,14 +354,15 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
                 pointsY.append(pt.y())
                 params.append(parameter)
 
-        self.writePoints(pointsX, pointsY, params, subsetName)
+            prevKey = key
+
+        if len(pointsX):
+            self.writePoints(pointsX, pointsY, params, paramName)
 
 
-    def writePoints(self, pointsX, pointsY, params, subsetName):
+    def writePoints(self, pointsX, pointsY, params, paramName):
         assert len(params)==len(pointsX), 'Parameters/points are mismatch'
-        setNameToSave = self.mSetLineEdit.text()
         subsetNo = self.maxMapSubsetNo+1
-        paramNo = self.maxMapSetParameterNo+1
 
         npX = np.asarray(pointsX, '>d').tostring()
         npY = np.asarray(pointsY, '>d').tostring()
@@ -331,62 +370,69 @@ class QgisSaveMapsetToPDS(QtGui.QDialog, FORM_CLASS):
         sql = ("insert into tig_map_subset (db_sldnid, tig_map_subset_name, tig_map_set_no, "
                "tig_map_subset_no, TIG_MAP_X, TIG_MAP_Y) "
                "values (TIG_MAP_SUBSET_SEQ.nextval, '{0}', {1}, {2}, :blobX, :blobY)"
-               .format(setNameToSave, self.groupNo, subsetNo))
-
-        sql1 = ("insert into tig_map_set_param (db_sldnid, tig_param_short_name, TIG_PARAM_LONG_NAME, "
-                "tig_map_set_no, tig_map_set_parameter_no ) "
-                "values (TIG_MAP_SET_PARAM_SEQ.nextval, '0', '{0}', {1}, {2})"
-                .format(subsetName, self.groupNo, paramNo ))
+               .format(paramName, self.groupNo, subsetNo))
 
         sql2 = ("insert into tig_map_subset_param_val (db_sldnid, "
                 "tig_map_set_no, tig_map_subset_no, tig_map_set_parameter_no, TIG_MAP_PARAM_VRLONG ) "
-                "values (TIG_MAP_SUBSET_PARAM_VAL_SEQ.nextval, '0', {0}, {1}, {2}, '{3}')"
-                .format(self.groupNo, subsetNo, paramNo, npZ))
+                "values (TIG_MAP_SUBSET_PARAM_VAL_SEQ.nextval, {0}, {1}, {2}, :paramZ)"
+                .format(self.groupNo, subsetNo, self.paramNo))
 
         cursor = self.db.cursor()
         blobvarX = cursor.var(cx_Oracle.BLOB)
         blobvarX.setvalue(0, npX)
         blobvarY = cursor.var(cx_Oracle.BLOB)
         blobvarY.setvalue(0, npY)
-        # cursor.setinputsizes(blobX=cx_Oracle.BLOB)
-        # cursor.setinputsizes(blobY=cx_Oracle.BLOB)
-        # cursor.execute(sql, {'blobX': blobvarX, 'blobY': blobvarY})
-        # cursor.execute('commit')
+        blobvarZ = cursor.var(cx_Oracle.BLOB)
+        blobvarZ.setvalue(0, npZ)
+
         self.db.execute(sql, blobX=blobvarX, blobY=blobvarY)
-        # self.db.execute(sql1)
-        # self.db.execute(sql2)
+        self.db.execute(sql2, paramZ=blobvarZ)
         self.db.commit()
 
+        self.maxMapSubsetNo = self.maxMapSubsetNo + 1
+
     def processAsMultiPoints(self):
+
         features = self.currentLayer.getFeatures()
         for f in features:
-            subsetName = 'Qgis'
+            paramName = 'Qgis'
             parameter = 0
             if self.subsetFieldIndex >= 0:
-                subsetName = f.attribute(self.subsetFieldName)
+                paramName = f.attribute(self.subsetFieldName)
             if self.parameterFieldIndex >= 0:
                 parameter = f.attribute(self.parameterFieldName)
-            print subsetName, parameter
 
             geom = f.geometry()
-            points = []
+            pointsX = []
+            pointsY = []
+            params = []
             t = geom.wkbType()
             if t == QGis.WKBMultiPoint:
                 mpt = geom.asMultiPoint()
                 for pt in mpt:
-                    points.append(self.xform.transform(pt))
-                print points
+                    newPt = self.xform.transform(pt)
+                    pointsX.append(newPt.x())
+                    pointsY.append(newPt.y())
+                    params.append(parameter)
+                self.writePoints(pointsX, pointsY, params, paramName)
             elif t == QGis.WKBLineString:
                 mpt = geom.asPolyline()
                 for pt in mpt:
-                    points.append(self.xform.transform(pt))
-                print points
+                    newPt = self.xform.transform(pt)
+                    pointsX.append(newPt.x())
+                    pointsY.append(newPt.y())
+                    params.append(parameter)
+                self.writePoints(pointsX, pointsY, params, paramName)
             elif t == QGis.WKBPolygon:
                 mpt = geom.asPolygon()
                 for polyline in mpt:
                     for pt in polyline:
-                        points.append(self.xform.transform(pt))
-                print points
+                        newPt = self.xform.transform(pt)
+                        pointsX.append(newPt.x())
+                        pointsY.append(newPt.y())
+                        params.append(parameter)
+                    self.writePoints(pointsX, pointsY, params, paramName)
+
 
 
 
