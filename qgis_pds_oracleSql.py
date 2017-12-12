31e1d2f3e13c45baa1c8b46544d47856ff8aa39b
vvkond <kondrashov.viktor@gmail.com>
2017-12-12 23:43:11 +0500

    Oracle SQL

--- qgis_pds_oracleSql.py
+++ qgis_pds_oracleSql.py
@@ -26,16 +26,74 @@ class QgisOracleSql(QtGui.QDialog, FORM_CLASS):
 
         self.setupUi(self)
 
+        self.project = project
+        self.iface = iface
+
+        if not self.initDb():
+            return
+
+        self.setWindowTitle(self.windowTitle() + ' - ' + self.project['project'])
+
         self.mRefreshToolButton.setIcon(QIcon(u':/plugins/QgisPDS/Refresh.png'))
-        self.mSelectFileToolButton.setIcon(QIcon(u':/plugins/QgisPDS/openfileicon.png'))
         self.mExecuteToolButton.setIcon(QIcon(u':/plugins/QgisPDS/ButtonPlayicon.png'))
 
+    def initDb(self):
+        if self.project is None:
+            self.iface.messageBar().pushMessage(self.tr("Error"),
+                                                self.tr(u'No current PDS project'), level=QgsMessageBar.CRITICAL)
+
+            return False
+
+        connection = create_connection(self.project)
+        scheme = self.project['project']
+        try:
+            self.db = connection.get_db(scheme)
+            self.tig_projections = TigProjections(db=self.db)
+            proj = self.tig_projections.get_projection(self.tig_projections.default_projection_id)
+            if proj is not None:
+                self.proj4String = 'PROJ4:' + proj.qgis_string
+                destSrc = QgsCoordinateReferenceSystem()
+                destSrc.createFromProj4(proj.qgis_string)
+                sourceCrs = QgsCoordinateReferenceSystem('epsg:4326')
+                self.xform = QgsCoordinateTransform(sourceCrs, destSrc)
+        except Exception as e:
+            self.iface.messageBar().pushMessage(self.tr("Error"),
+                                                self.tr(u'Project projection read error {0}: {1}').format(
+                                                    scheme, str(e)),
+                                                level=QgsMessageBar.CRITICAL)
+            return False
+        return True
+
     def selectFileClicked(self):
+        lastFilePath = QSettings().value('PDS/OracleSQL/lastFilePath', u'.')
         fname = QFileDialog.getOpenFileName(self, u'Выбрать файл',
-                                            u'.', u"Файлы SQL (*.sql *.SQL);;Все файлы (*.*)")
+                                            lastFilePath, u"Файлы SQL (*.sql *.SQL);;Все файлы (*.*)")
+        if fname:
+            self.loadFile(fname)
+            self.refreshFields()
+
+            QSettings().setValue('PDS/OracleSQL/lastFilePath', os.path.dirname(fname))
+            self.mFileNameLineEdit.setText(fname)
+            if not self.mLayerNameLineEdit.text():
+                self.mLayerNameLineEdit.setText(os.path.basename(os.path.splitext(fname)[0]))
 
     def refreshClicked(self):
         pass
 
     def executeClicked(self):
         pass
+
+    def loadFile(self, fileName):
+        try:
+            with open(fileName, 'rb') as f:
+                text = f.read().decode('utf-8')
+                self.mSqlTextEdit.setPlainText(text)
+        except:
+            pass
+
+    def refreshFields(self):
+        sql = self.mSqlTextEdit.toPlainText()
+        rows = self.db.execute_assoc(sql)
+        for row in rows:
+            print row.keys()
+            break
