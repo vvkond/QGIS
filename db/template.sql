4b563e9110710457b7f272bf73634743397fffb0
Alexey Russkikh <skylex72rus@gmail.com>
2018-12-18 18:39:47 +0500

    Работа со списками. -Список пользователей берется не из схемы с проектом а из global

--- db/template.sql
+++ db/template.sql
@@ -5,7 +5,7 @@ SELECT
     i.LOGIN_NAME,
     t.DB_INSTANCE_TIME_STAMP / 86400 "Created"
 FROM
-    (select TIG_USER_ID, max(TIG_LOGIN_NAME) as login_name from tig_interpreter group by TIG_USER_ID) i
+    (select TIG_USER_ID, max(TIG_LOGIN_NAME) as login_name from GLOBAL.tig_interpreter group by TIG_USER_ID) i
     left join tig_template t on t.TIG_INTERPRETER_SLDNID = i.TIG_USER_ID
 WHERE
     t.TIG_APPLICATION_NAME = :app_name
