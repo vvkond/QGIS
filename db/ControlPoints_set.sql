76ce16adf09202f64605dfc6fa6c0f74b2166577
Alexey Russkikh <skylex72rus@gmail.com>
2019-01-30 15:34:10 +0500

    Запросы на группы/наборы КТ. -убрал пока проверку на наличие данных поскольку текущий запрос не оптимальный,медленный

--- db/ControlPoints_set.sql
+++ db/ControlPoints_set.sql
@@ -11,18 +11,18 @@ WHERE
     AND(ms.TIG_MAP_SET_NO = :group_id
     OR :group_id IS NULL)
     AND ms.TIG_MAP_SET_TYPE = 1
-    AND EXISTS
-    (SELECT
-        *
-    FROM
-        TIG_MAP_SUBSET mss,
-        TIG_MAP_SUBSET_PARAM_VAL mssp
-    WHERE
-        mssp.TIG_MAP_SUBSET_NO = mss.TIG_MAP_SUBSET_NO
-        AND mssp.TIG_MAP_SET_PARAMETER_NO = msp.TIG_MAP_SET_PARAMETER_NO
-        AND ms.TIG_MAP_SET_NO = mss.TIG_MAP_SET_NO
-        AND ms.TIG_MAP_SET_NO = mssp.TIG_MAP_SET_NO
-    )
+---    AND EXISTS
+---    (SELECT
+---        *
+---    FROM
+---        TIG_MAP_SUBSET mss,
+---        TIG_MAP_SUBSET_PARAM_VAL mssp
+---    WHERE
+---        mssp.TIG_MAP_SUBSET_NO = mss.TIG_MAP_SUBSET_NO
+---        AND mssp.TIG_MAP_SET_PARAMETER_NO = msp.TIG_MAP_SET_PARAMETER_NO
+---        AND ms.TIG_MAP_SET_NO = mss.TIG_MAP_SET_NO
+---        AND ms.TIG_MAP_SET_NO = mssp.TIG_MAP_SET_NO
+---    )
 ORDER BY
     ms.TIG_MAP_SET_NAME,
     msp.TIG_PARAM_LONG_NAME
