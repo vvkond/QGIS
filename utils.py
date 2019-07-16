764611d050faf1bbeac798ac5a254100abed2236
Alexey Russkikh <skylex72rus@gmail.com>
2019-07-16 17:17:02 +0500

    #Feature #utils Функция which на подобии линуксовой

--- utils.py
+++ utils.py
@@ -349,7 +349,25 @@ def ping(host,hidden=True):
     return subprocess.call(command, startupinfo=startupinfo) == 0
 
 
-
+#===============================================================================
+# This function is like the linux which command
+#===============================================================================
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        #for path in os.environ["PATH"].split(os.pathsep):
+        for path in getenv_system("PATH").split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
 #===============================================================================
 # 
 #===============================================================================
