3f8be0a42250a017258efd1479b56b9c2a62236c
Alexey Russkikh <skylex72rus@gmail.com>
2019-08-13 15:24:41 +0500

    #Deviation numpy.fromstring->numpy.frombuffer

--- qgis_pds_deviation.py
+++ qgis_pds_deviation.py
@@ -277,8 +277,8 @@ class QgisPDSDeviation(QObject, WithQtProgressBar):
                     startY = pt.y()
                     polyLine = [pt]
 
-                    blob_x = numpy.fromstring(self.db.blobToString(row[21]), '>f').astype('d')
-                    blob_y = numpy.fromstring(self.db.blobToString(row[22]), '>f').astype('d')
+                    blob_x = numpy.frombuffer(self.db.blobToString(row[21]), '>f').astype('d')
+                    blob_y = numpy.frombuffer(self.db.blobToString(row[22]), '>f').astype('d')
                     for ip in xrange(len(blob_x)):
                         dx = blob_x[ip]
                         dy = blob_y[ip]
