8069deadd224704cdf48995fddc327ca4a0bf2c1
Alexey Russkikh <skylex72rus@gmail.com>
2019-10-09 15:47:22 +0500

    #Deviation Упрощение кривой. Удаление лишних точек

--- qgis_pds_deviation.py
+++ qgis_pds_deviation.py
@@ -19,8 +19,55 @@ from bblInit import STYLE_DIR, Fields
 from utils import load_styles_from_dir, load_style, plugin_path, edit_layer,\
     WithQtProgressBar
 
+
+#===============================================================================
+# Line simplification. Not fast...
+#===============================================================================
+def _vec2d_dist(p1, p2):
+    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+
+def _vec2d_sub(p1, p2):
+    return (p1[0]-p2[0], p1[1]-p2[1])
+
+def _vec2d_mult(p1, p2):
+    return p1[0]*p2[0] + p1[1]*p2[1]
+
+def ramerdouglas(line, dist):
+    """Does Ramer-Douglas-Peucker simplification of a curve with `dist`
+    threshold.
+    `line` is a list-of-tuples, where each tuple is a 2D coordinate
+    Usage is like so:
+    >>> myline = [(0.0, 0.0), (1.0, 2.0), (2.0, 1.0)]
+    >>> simplified = ramerdouglas(myline, dist = 1.0)
+    """
+    if len(line) < 3:
+        return line
+    (begin, end) = (line[0], line[-1]) if line[0] != line[-1] else (line[0], line[-2])
+    distSq = []
+    for curr in line[1:-1]:
+        if float(_vec2d_dist(begin, end))<>float(0):
+            tmp = (
+                _vec2d_dist(begin, curr) - _vec2d_mult(_vec2d_sub(end, begin), _vec2d_sub(curr, begin)) ** 2 / _vec2d_dist(begin, end)
+                  )
+            distSq.append(tmp)
+    if len(distSq)>0:            
+        maxdist = max(distSq)
+        if maxdist < dist ** 2:
+            return [begin, end]
+    else:
+        return [begin, end]
+    pos = distSq.index(maxdist)
+    return (ramerdouglas(line[:pos + 2], dist) + 
+            ramerdouglas(line[pos + 1:], dist)[1:])
+
+#===============================================================================
+# 
+#===============================================================================
 class QgisPDSDeviation(QObject, WithQtProgressBar):
-    def __init__(self, iface, project  ,styleName=None ,styleUserDir=None):
+    def __init__(self, iface, project  ,styleName=None ,styleUserDir=None,simplificationDistance=0.1):
+        '''
+            @param simplificationDistance: if not None then use Ramer-Douglas-Peucker simplification of a curve with that distance
+        '''
         super(QgisPDSDeviation, self).__init__()
 
         self.plugin_dir = os.path.dirname(__file__)
@@ -58,6 +105,8 @@ class QgisPDSDeviation(QObject, WithQtProgressBar):
         
         self.styleName=styleName
         self.styleUserDir=styleUserDir
+        
+        self.simplificationDistance=simplificationDistance
         
 
     def initDb(self):
@@ -279,12 +328,15 @@ class QgisPDSDeviation(QObject, WithQtProgressBar):
 
                     blob_x = numpy.frombuffer(self.db.blobToString(row[21]), '>f').astype('d')
                     blob_y = numpy.frombuffer(self.db.blobToString(row[22]), '>f').astype('d')
-                    for ip in xrange(len(blob_x)):
-                        dx = blob_x[ip]
-                        dy = blob_y[ip]
+                    
+                    
+                    line=numpy.stack([blob_x,blob_y]).T.tolist()
+                    if self.simplificationDistance:
+                        line=ramerdouglas(line=line,dist=self.simplificationDistance)
+                    for (dx,dy) in line:
                         pt = QgsPoint(startX+dx, startY+dy)
                         polyLine.append(pt)
-                    pt = QgsPoint(pt.x()+0.00000001, pt.y()+0.00000001)
+                    pt = QgsPoint(pt.x()+0.00000001, pt.y()+0.00000001) #special offset for draw wel without deviation
                     polyLine.append(pt)
 
                     geom = QgsGeometry.fromPolyline(polyLine)
